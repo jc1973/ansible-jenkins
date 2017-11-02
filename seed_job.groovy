caad2aabb4b953b1f8e5a10a97e93a5f95ca283c
John Carter <john.a.carter@capgemini.com>
2017-11-02 11:35:12 +0000

    get working directory of script

--- seed_job.groovy
+++ seed_job.groovy
@@ -1,10 +1,22 @@
-new File("jobs").eachFile() { file->  
-//  println file.getName();
-  //# evaluate(new File(file.getName())
-  def filename = "jobs/" + file.getName()
-  
-  // evaluate(new File(file.getName()))
+def script_dir = new File(getClass().protectionDomain.codeSource.location.path).parent
+def jobs_dir = script_dir + "/jobs"
+
+//new File("jobs").eachFile() { file->  
+new File(jobs_dir).eachFile() { file->  
+  println file.getName();
+  def filename = jobs_dir + "/" + file.getName()
+  println filename
   evaluate(new File(filename))
+
+
+//   
+//   //  println file.getName();
+//     //# evaluate(new File(file.getName())
+//     def filename = "jobs/" + file.getName()
+//     
+//     // evaluate(new File(file.getName()))
+//     evaluate(new File(filename))
+//     evaluate(new File(filename))
 
 }  
 
